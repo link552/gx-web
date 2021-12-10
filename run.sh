e981276de1ef6e2b29cf03e5fd570992a6b6ef2c
Kyle L <linkageneb552@hotmail.com>
2021-12-09 20:56:49 -0500

    Creating build directory.

--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# if ! gx main.gx.go main ; then
-#     exit
-# fi
+mkdir -p build
 
 if ! gx example/main.gx.go build/main ; then
     exit
 fi
 
 echo "#include <cstdlib>" | cat - build/gx.hh > build/temp && mv build/temp build/gx.hh
-
-# if ! g++ main.gx.cc cppfpm.cpp -lfcgi++ -lfcgi -std=c++17 -o hello_world ; then
-#     exit
-# fi
 
 export CPATH=/gx-web/build:/gx-web/gx-ext:/gx-web/cpp-fpm
 
